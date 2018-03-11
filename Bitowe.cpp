f956f09246ae056801a2d737919f05c8e080d2ff
paolo21d <paolo21d@gmail.com>
2018-03-11 13:22:54 +0100

    Utworzenie plików

---
+++ Bitowe.cpp
@@ -0,0 +1,7 @@
+//
+// Created by paolo on 11.03.2018.
+//
+
+#include "Bitowe.h"
+#include <bitset>
+#include <iostream>
---
+++ Bitowe.h
@@ -0,0 +1,16 @@
+//
+// Created by paolo on 11.03.2018.
+//
+
+#ifndef PROI_P1_BITOWE_H
+#define PROI_P1_BITOWE_H
+
+#include <bitset>
+#include <iostream>
+
+class Bitowe {
+
+};
+
+
+#endif //PROI_P1_BITOWE_H
